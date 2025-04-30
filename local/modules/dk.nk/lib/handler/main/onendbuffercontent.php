d5070eb1833bb1a3bf9cf5ea9d32ee7911686910
d.kostenko <d.kostenko@bp-solutions.ru>
2025-04-30 23:11:50 +0300

    buffer update

--- local/modules/dk.nk/lib/handler/main/onendbuffercontent.php
+++ local/modules/dk.nk/lib/handler/main/onendbuffercontent.php
@@ -7,10 +7,10 @@ class OnEndBufferContent
 
     public static function run(&$content): void
     {
-//        $pattern = '/(<link[^>]+rel=["\']canonical["\'][^>]+href=["\']https?:\/\/[^"\']*?\/)page-\d+\/([^"\']*["\'][^>]*>)/iu';
-//        $replacement = '$1$2';
-//        $newContent = preg_replace($pattern, $replacement, $content, -1, $count);
-//        if ($count) $content = $newContent;
+        $pattern = '/(<link[^>]+rel=["\']canonical["\'][^>]+href=["\']https?:\/\/[^"\']*?\/)page-\d+\/([^"\']*["\'][^>]*>)/iu';
+        $replacement = '$1$2';
+        $newContent = preg_replace($pattern, $replacement, $content, -1, $count);
+        if ($count) $content = $newContent;
     }
 
 }
